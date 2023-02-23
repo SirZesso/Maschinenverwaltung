12725d22a03dd2316e7537c12cd98b5f7690ab6e
Yannic Ziegler <32840988+SirZesso@users.noreply.github.com>
2023-02-23 22:40:47 +0100

    SerializationServiceTest integriert

--- src/test/java/service/SerializationServiceTest.java
+++ src/test/java/service/SerializationServiceTest.java
@@ -1,47 +1,43 @@
-// package service;
+package service;
 
-// import org.junit.jupiter.api.Assertions;
-// import org.junit.jupiter.api.Test;
-// import javafx.collections.FXCollections;
-// import javafx.collections.ObservableList;
-// import model.Enterprise;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Enterprise;
 
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-// public class SerializationServiceTest {
+public class SerializationServiceTest {
 
-// private final String FILE_NAME = "EnterpriseTest.ser";
+    private final String FILE_NAME = "EnterpriseTest.ser";
 
-// @Test
-// public void testSerializeAndDeserializeEnterpriseData() {
+    @Test
+    public void testSerializeAndDeserializeEnterpriseData() {
 
-// List<Enterprise> enterpriseList = new ArrayList<>();
-// enterpriseList.add(new Enterprise("Enterprise 1", "logo1.png", null));
-// enterpriseList.add(new Enterprise("Enterprise 2", "logo2.png", null));
+        List<Enterprise> enterpriseList = new ArrayList<>();
+        enterpriseList.add(new Enterprise("Enterprise 1", "logo1.png", null));
+        enterpriseList.add(new Enterprise("Enterprise 2", "logo2.png", null));
 
-// ObservableList<Enterprise> enterprises =
-// FXCollections.observableList(enterpriseList);
+        ObservableList<Enterprise> enterprises = FXCollections.observableList(enterpriseList);
 
-// // Serialize test data
-// SerializationService.serializeEnterpriseData(enterprises, FILE_NAME);
+        // Serialize test data
+        SerializationService.serializeEnterpriseData(enterprises, FILE_NAME);
 
-// // Deserialize test data
-// ObservableList<Enterprise> deserializedEnterprises =
-// SerializationService.deSerializeEnterpriseDatao(FILE_NAME);
+        // Deserialize test data
+        ObservableList<Enterprise> deserializedEnterprises = SerializationService.deSerializeEnterpriseDatao(FILE_NAME);
 
-// // Check if deserialized data matches original data
-// Assertions.assertEquals(enterprises.size(), deserializedEnterprises.size(),
-// "Number of enterprises does not match.");
+        // Check if deserialized data matches original data
+        Assertions.assertEquals(enterprises.size(), deserializedEnterprises.size(),
+                "Number of enterprises does not match.");
 
-// for (int i = 0; i < enterprises.size(); i++) {
-// Enterprise original = enterprises.get(i);
-// Enterprise deserialized = deserializedEnterprises.get(i);
+        for (int i = 0; i < enterprises.size(); i++) {
+            Enterprise original = enterprises.get(i);
+            Enterprise deserialized = deserializedEnterprises.get(i);
 
-// Assertions.assertEquals(original.getId(), deserialized.getId(), "ID does not
-// match.");
-// Assertions.assertEquals(original.getName(), deserialized.getName(), "Name
-// does not match.");
-// }
-// }
-// }
+            Assertions.assertEquals(original.getId(), deserialized.getId(), "ID does not match.");
+            Assertions.assertEquals(original.getName(), deserialized.getName(), "Name does not match.");
+        }
+    }
+}
